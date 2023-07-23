aa3875e3f8f829287395c59a24f1881264f2a453
Nagaraju Matta <nagaraju.matta@infomagnus.com>
2023-07-23 17:17:49 +0530

    Addng python language translation changes

---
+++ workspaces/Python/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+NODE_ENV = os.getenv("NODE_ENV")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+is_prod = NODE_ENV == "production"
+
+PAYPAL_API_BASE = "https://api.paypal.com" if is_prod else "https://api.sandbox.paypal.com"
