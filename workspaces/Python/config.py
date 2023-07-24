91a5bd7cec610abc1bea3101e6904d0b91756dd8
Kalpana Reddy <Kalpana.c@infomagnus.com>
2023-07-24 20:16:15 +0530

    Changed env to development

--- workspaces/Python/config.py
+++ workspaces/Python/config.py
@@ -8,7 +8,7 @@ NODE_ENV = os.getenv("NODE_ENV")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-is_prod = NODE_ENV == "production"
+is_prod = NODE_ENV == "development"
 
 PAYPAL_API_BASE = "https://api.paypal.com" if is_prod else "https://api.sandbox.paypal.com"
 
